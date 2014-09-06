80380b1ea9ec442a68e90a46f4dd220b31ed6a9b
crywolfe <gwolfe2007@gmail.com>
2014-09-06 13:23:01 -0500

    CSV read work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
 
+  def read_csv(file_location)
+
+
+    CSV.foreach(file_location) do |row|
+      ...
+    end
+
+    # OR USE array = CSV.read(file_location")
+
+  end
+
 
 end
